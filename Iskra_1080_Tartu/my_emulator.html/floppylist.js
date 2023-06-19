8f39fa13609da380212b82301d7cbb3fa53ede04
Alemorf <aleksey.f.morozov@gmail.com>
2023-06-19 16:25:11 +0300

    -

--- Iskra_1080_Tartu/my_emulator.html/floppylist.js
+++ Iskra_1080_Tartu/my_emulator.html/floppylist.js
@@ -1,5 +1,6 @@
 const floppyList = [
-    {file : "simple", text : "Small programms"},
+    {file : "small", text : "Маленькие программы"},
     {file : "sc2", text : "Supercalc 2"},
+    {file : "ws", text : "Word star"},
     {file : "network", text : "Для загрузки из локальной сети"},
 ];
